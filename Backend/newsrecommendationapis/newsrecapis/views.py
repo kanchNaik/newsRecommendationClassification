3e690ac568d90d08ba10784bc964c7e9b2da7d2b
mrunalikatta1998 <mrunali.katta2@gmail.com>
2024-11-30 18:20:39 -0800

    Update views.py

--- Backend/newsrecommendationapis/newsrecapis/views.py
+++ Backend/newsrecommendationapis/newsrecapis/views.py
@@ -1,13 +1,20 @@
 from django.http import JsonResponse
-from newsrecapis.MLModels.NaiveBayse import train_nb_model
-
+from Backend.newsrecommendationapis.newsrecapis.MLModels.NaiveBayse import train_nb_model
+from Backend.newsrecommendationapis.newsrecapis.MLModels.LogisticRegression import train_lr_model
+from Backend.newsrecommendationapis.newsrecapis.MLModels.SVM import train_svc_model
 def trainModel(request):
     try:
         train_nb_model()  # Call the function to train the model
         return JsonResponse({"message": "Model training successful"}, status=200)
+
+        train_lr_model()  # Call the function to train the model
+        return JsonResponse({"message": "Model training successful"}, status=200)
+
+        train_svc_model()  # Call the function to train the model
+        return JsonResponse({"message": "Model training successful"}, status=200)
+
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
-    
 
 def getRecommendedNews(request):
     # Implement logic to get recommended news based on the category
