9e7486fa2b357f28747b655005e112531875b104
Prasad-shimpatwar <prasadshimpatwar26@gmail.com>
2024-12-01 10:44:07 +0530

    Update NewsClassProcessor.py

--- Backend/newsrecommendationapis/newsrecapis/News/NewsClassProcessor.py
+++ Backend/newsrecommendationapis/newsrecapis/News/NewsClassProcessor.py
@@ -1,23 +1,59 @@
-from newsrecapis.News.fetchnews import fetch_news_article, get_newsText
-from newsrecapis.dataprep import get_tfidf
+from Backend.newsrecommendationapis.newsrecapis.News.fetchnews import fetch_news_article, get_newsText
+from Backend.newsrecommendationapis.newsrecapis.dataprep import get_tfidf
 import pickle
 
-
-
-
-def get_newsWithClass():
+def get_newsWithClass_nb():
     model = None
 
     with open('newsrecapis/MLModels/picklefilesofmodels/nb_model_combinedcat.pkl', 'rb') as f:
         model = pickle.load(f)
-
 
     vectorizer = None
     with open("newsrecapis/MLModels/picklefilesofmodels/tfidf_vectorizer.pkl", 'rb') as f:
         vectorizer = pickle.load(f)
     newsArticles = fetch_news_article()
     newsText = get_newsText(newsArticles)
-    
+
+    vectors = vectorizer.transform(newsText['text'])
+    predictions = model.predict(vectors)
+
+    for article, prediction in zip(newsArticles, predictions):
+        article['prediction'] = prediction
+
+    return newsArticles
+
+def get_newsWithClass_lr():
+    model = None
+
+    with open('newsrecapis/MLModels/picklefilesofmodels/lr_model_combinedcat.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    vectorizer = None
+    with open("newsrecapis/MLModels/picklefilesofmodels/tfidf_vectorizer.pkl", 'rb') as f:
+        vectorizer = pickle.load(f)
+    newsArticles = fetch_news_article()
+    newsText = get_newsText(newsArticles)
+
+    vectors = vectorizer.transform(newsText['text'])
+    predictions = model.predict(vectors)
+
+    for article, prediction in zip(newsArticles, predictions):
+        article['prediction'] = prediction
+
+    return newsArticles
+
+def get_newsWithClass_svc():
+    model = None
+
+    with open('newsrecapis/MLModels/picklefilesofmodels/svc_model_combinedcat.pkl', 'rb') as f:
+        model = pickle.load(f)
+
+    vectorizer = None
+    with open("newsrecapis/MLModels/picklefilesofmodels/tfidf_vectorizer.pkl", 'rb') as f:
+        vectorizer = pickle.load(f)
+    newsArticles = fetch_news_article()
+    newsText = get_newsText(newsArticles)
+
     vectors = vectorizer.transform(newsText['text'])
     predictions = model.predict(vectors)
 
