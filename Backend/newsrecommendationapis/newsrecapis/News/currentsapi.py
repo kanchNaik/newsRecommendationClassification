bbb6f60b837c201bc42977399a4e0a23c43e320d
Kanchan Ashok Naik <167585299+kanchNaik@users.noreply.github.com>
2025-03-05 19:08:53 -0800

    Update currentsapi.py

--- Backend/newsrecommendationapis/newsrecapis/News/currentsapi.py
+++ Backend/newsrecommendationapis/newsrecapis/News/currentsapi.py
@@ -2,7 +2,7 @@ import requests
 
 class CurrentsapiAPI:
     def get_news(self, page):
-        base_url = f"https://api.currentsapi.services/v1/latest-news?&language=us&apiKey=pe8yIYK8I-IJyzSU3vv6BX0lv-kKOvxn8Yw6QWO2gLnijs2N"
+        base_url = f"https://api.currentsapi.services/v1/latest-news?&language=us&apiKey="
         response = requests.get(base_url)
         response.raise_for_status() 
         data = response.json()
